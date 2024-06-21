d3c7e689edc4d9ac6eaa90e623d9ee4e66831026
kulde <ku1de@icloud.com>
2024-06-21 14:24:00 +0200

    Add: series property to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-import { src, dest, watch } from "gulp";
+import { src, dest, watch, series } from "gulp";
 import * as dartSass from "sass";
 import gulpSass from "gulp-sass";
 
@@ -19,3 +19,5 @@ export function css(done) {
 export function dev() {
   watch("src/scss/**/*.scss", css);
 }
+
+export default series(js, css, dev);
