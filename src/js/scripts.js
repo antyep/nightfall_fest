3245474df9a6e29fdf320a4a16bd07c3e27db2f8
kulde <ku1de@icloud.com>
2024-06-21 18:41:11 +0200

    Add: const for image quantity

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,9 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function createGallery() {
+  const IMAGE_QUANTITY = 16;
   const gallery = document.querySelector(".gallery-img");
 
-  for (let i = 1; i <= 16; i++) {
+  for (let i = 1; i <= IMAGE_QUANTITY; i++) {
     const image = document.createElement("IMG");
     image.src = `./src/img/gallery/event/${i}.jpg`;
     image.alt = "gallery img";
