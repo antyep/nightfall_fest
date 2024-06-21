27881a602849edcfba8195b8e46e05bd0f8fefa8
kulde <ku1de@icloud.com>
2024-06-21 19:30:40 +0200

    Update: showModal loop fix

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,6 +7,10 @@ function createGallery() {
   const gallery = document.querySelector(".gallery-img");
 
   for (let i = 1; i <= IMAGE_QUANTITY; i++) {
+    const image = document.createElement("IMG");
+    image.src = `./src/img/gallery/event/${i}.jpg`;
+    image.alt = "gallery img";
+
     image.onclick = function () {
       showImage(i);
     };
@@ -22,6 +26,7 @@ function createGallery() {
     const modal = document.createElement("DIV");
     modal.classList.add("modal");
     modal.onclick = closeModal;
+
     modal.appendChild(image);
 
     const body = document.querySelector("body");
